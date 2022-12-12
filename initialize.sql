4b5002ef53d777b1779037f86b249e866ee106ea
Abby Chen <95500106+abbyrchen@users.noreply.github.com>
2022-12-11 21:36:58 -0500

    Update initialize.sql

--- initialize.sql
+++ initialize.sql
@@ -21,13 +21,13 @@ CREATE database ebay;
 
 \copy Sellers(seller_id) FROM sellers.csv csv header;
 
-\copy Products(product_id, product_name, price, amount, category, seller_id) FROM products.csv csv header;
+\copy Products(product_id, product_name, price, amount_in_stock, category, seller_id) FROM products.csv csv header;
 
-\copy Transactions(transaction_id,date,price,amount,customer_id,product_id) FROM transactions.csv csv header;
+\copy Transactions(transaction_id,date,amount,customer_id,product_id) FROM transactions.csv csv header;
 
 \copy Auctions(transaction_id, min_auc_price, time_duration) FROM auctions.csv csv header;
 
-\copy Bids(bid_id, auction_id, amount) FROM bids.csv csv header;
+\copy Bids(bid_id, auction_id, bidded_amount) FROM bids.csv csv header;
 
 \copy Reviews(review_id,rating,description,product_id) FROM reviews.csv csv header;
 
